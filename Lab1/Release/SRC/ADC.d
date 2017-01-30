cdc5ef0cf298617d89f2f504c26cdf891d028bd1
osh1996 <osh1996@gmail.com>
2017-01-30 00:50:43 -0500

    Add files via upload

---
+++ Lab1/Release/SRC/ADC.d
@@ -0,0 +1,46 @@
+SRC/ADC.d SRC/ADC.o: ../SRC/ADC.c \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/RBELib.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/ADC.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/DAC.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/Debug.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/motors.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/USARTDebug.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/timer.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/Periph.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/pot.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/PID.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/reg_structs.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/ports.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/SPI.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/SetServo.h \
+  C:\Users\ttruong\Documents\RBELib\include/RBELib/SlaveSelects.h
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/RBELib.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/ADC.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/DAC.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/Debug.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/motors.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/USARTDebug.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/timer.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/Periph.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/pot.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/PID.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/reg_structs.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/ports.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/SPI.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/SetServo.h:
+
+C:\Users\ttruong\Documents\RBELib\include/RBELib/SlaveSelects.h:
---
+++ Lab1/Release/SRC/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../SRC/ADC.c \
+../SRC/DAC.c \
+../SRC/SPI.c \
+../SRC/USARTDebug.c \
+../SRC/main.c \
+../SRC/timer.c 
+
+OBJS += \
+./SRC/ADC.o \
+./SRC/DAC.o \
+./SRC/SPI.o \
+./SRC/USARTDebug.o \
+./SRC/main.o \
+./SRC/timer.o 
+
+C_DEPS += \
+./SRC/ADC.d \
+./SRC/DAC.d \
+./SRC/SPI.d \
+./SRC/USARTDebug.d \
+./SRC/main.d \
+./SRC/timer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+SRC/%.o: ../SRC/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -I"C:\Users\ttruong\Documents\RBELib\include" -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega644p -DF_CPU=18432000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
